b31dc1f8dacfdb3425c15e716e272c8ad38e2bbe
Annie <annmora21@gmail.com>
2025-12-10 19:53:17 -0800

    Update

--- final-exam-I.cpp
+++ final-exam-I.cpp
@@ -1,2 +1,23 @@
 // COMSC-210 | Final Exam I | Annie Morales
 // IDE used: Visual Studio Code
+
+#include <iostream>
+#include <fstream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    ifstream file;
+    file.open("210-final-FA25.txt");
+    if (!file.good()) throw "Error: Failed to open the file.";
+
+    map<string, int> airport;
+    string origin;
+    int num;
+    while (file >> origin >> num) {
+        
+    }
+
+    return 0;
+}
