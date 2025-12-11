62412b4878398671c27088243bfb9c8e944e5aeb
Annie <annmora21@gmail.com>
2025-12-10 20:10:50 -0800

    Milestone 1

--- final-exam-I.cpp
+++ final-exam-I.cpp
@@ -9,15 +9,22 @@ using namespace std;
 int main()
 {
     ifstream file;
-    file.open("210-final-FA25.txt");
+    file.open("210-final-1-FA25.txt");
     if (!file.good()) throw "Error: Failed to open the file.";
 
     map<string, int> airport;
     string origin;
+    string count;
     int num;
-    while (file >> origin >> num) {
-        
+    while (file >> origin >> count) {
+        airport[origin]++;
+        airport[count]++;
     }
+    file.close();
 
+    cout << "Airport Information: \n";
+    for (const auto& port : airport) {
+        cout << port.first << " " << port.second << endl;
+    }
     return 0;
 }
